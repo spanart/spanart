a1680cd984dd924dc75ce1c8dce29c96dfd696ac
Sneyden <sp@anart.co>
2021-06-07 19:36:53 +0530

    updated accounts ui

--- src/app/internal/accounts/accounts.component.ts
+++ src/app/internal/accounts/accounts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/service/auth/auth.service';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-accounts',
@@ -11,10 +12,21 @@ export class AccountsComponent implements OnInit {
   constructor(private authService: AuthService) { }
 
   user: string = '';
+  faSearch = faSearch;
+
+  count = 13;
+
+  cards: number[] = [];
+
 
   ngOnInit(): void {
     const authDetails = this.authService.getAuthDetails();
     this.user = authDetails.userId;
+
+    
+    for (let index = 1; index <= this.count; index++) {
+      this.cards.push(index);
+    }
   }
 
   logout() {
