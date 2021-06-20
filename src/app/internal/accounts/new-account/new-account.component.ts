6953a2ad259421383627f658b7ed37cfbb2d4619
spanart <sp@anart.co>
2021-06-20 22:22:47 +0530

    Added custom stepper

--- src/app/internal/accounts/new-account/new-account.component.ts
+++ src/app/internal/accounts/new-account/new-account.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import {CdkStepper} from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.scss']
+  styleUrls: ['./new-account.component.scss'],
+  providers: [{provide: CdkStepper, useExisting: NewAccountComponent}]
 })
 export class NewAccountComponent implements OnInit {
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,23 +4,29 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LogoScreenComponent } from './logo-screen/logo-screen.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
+import { StepperComponent } from './stepper/stepper.component';
+import { CdkStepperModule } from '@angular/cdk/stepper';
 
 
 @NgModule({
   declarations: [
-    LogoScreenComponent
+    LogoScreenComponent,
+    StepperComponent
   ],
   imports: [
     CommonModule,
     FontAwesomeModule,
     FlexLayoutModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CdkStepperModule
   ],
   exports: [
     FontAwesomeModule,
     LogoScreenComponent,
     FlexLayoutModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    StepperComponent,
+    CdkStepperModule
   ]
 })
 export class SharedModule { }
